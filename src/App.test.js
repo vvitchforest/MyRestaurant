2d628f5b69262d9d6e9687519cf9287072f3e955
irina <irina.konovalova455@gmail.com>
2022-09-23 13:53:17 +0300

    lint

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
+// eslint-disable-next-line no-undef
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/learn react/i)
+  // eslint-disable-next-line no-undef
+  expect(linkElement).toBeInTheDocument()
+})

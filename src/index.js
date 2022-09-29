4e0341a4bc230b3f724726d8f17284cc81b93fe1
teemutir <teemu.tirkkonen@metropolia.fi>
2022-09-29 20:36:49 +0300

    Map code reworked (fixed issue with refreshing), current location added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+// import { LoadScript } from '@react-google-maps/api'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,23 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 //  import { Container } from '@mui/material'
-import { GoogleMap } from '@react-google-maps/api'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+import { Button } from '@mui/material'
 
 const Map = () => {
+  const [currentPos, setCurrentPos] = useState({})
+  const [checkClick, setClick] = useState(false)
   const mapStyles = {
     height: '95vh',
     width: '100%'
   }
-
   const defaultCenter = {
-    lat: 60.2199174072532, lng: 24.757443753545
+    lat: 60.21978930158246, lng: 24.757250617314764
   }
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  })
+  const [map, setMap] = React.useState(null)
 
-  return (
+  const onLoad = React.useCallback(function callback (map) {
+    const bounds = new window.google.maps.LatLngBounds(defaultCenter)
+    map.fitBounds(bounds)
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback (map) {
+    setMap(null)
+  }, [])
+  const getPos = position => {
+    if (navigator.geolocation) {
+      const currentPosition = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+      setCurrentPos(currentPosition)
+    }
+  }
+  const panToLocation = () => {
+    setClick(true)
+    navigator.geolocation.getCurrentPosition(getPos)
+  }
+  return isLoaded
+    ? (
         <GoogleMap
+          id='map'
           mapContainerStyle={mapStyles}
           zoom={13}
-          center={defaultCenter}
-        />
-  )
+          center={checkClick
+            ? currentPos
+            : defaultCenter}
+            onLoad={onLoad}
+            onUnmount={onUnmount}>
+            <Button onClick={panToLocation} style={{ paddingLeft: 250 }}>Current Location</Button>
+            { console.log(map) }
+           <></>
+          </GoogleMap>
+      )
+    : <></>
 }
 export default Map

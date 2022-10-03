5f9dba5cbec4476c7bfbc9eb33446777ff40f42d
oskar wiiala <oskar.wiiala@metropolia.fi>
2022-10-03 16:31:22 +0300

    restaurant icons bug fix

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box } from '@mui/material'
+import { Box } from "@mui/material";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 import Drawer from "../components/Drawer";
 import "../Drawer.css";
@@ -11,25 +11,25 @@ import { IconButton } from "@mui/material";
 import { Button } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 // import RestaurantIcon from '@mui/icons-material/Restaurant'
-import MyLocationIcon from '@mui/icons-material/MyLocation'
+import MyLocationIcon from "@mui/icons-material/MyLocation";
 
 const Map = () => {
   const [currentPos, setCurrentPos] = useState({});
   const [checkClick, setClick] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-  const [checkNextPage, setNextPage] = useState(false)
-  const [libraries] = useState(['places', 'geometry'])
-  const placesList = []
+  const [checkNextPage, setNextPage] = useState(false);
+  const [libraries] = useState(["places", "geometry"]);
+  const placesList = [];
   // let getNextPage
-  const [placesFinal, setPlacesFinal] = useState([])
+  const [placesFinal, setPlacesFinal] = useState([]);
   const mapStyles = {
     height: "95vh",
     width: "100%",
   };
   const defaultCenter = {
     lat: 60.21978930158246,
-    lng: 24.757250617314764
-  }
+    lng: 24.757250617314764,
+  };
   const styles = {
     hide: [
       {
@@ -62,39 +62,39 @@ const Map = () => {
         featureType: "transit",
         stylers: [
           {
-            visibility: 'off'
-          }
-        ]
-      }
-    ]
-  }
+            visibility: "off",
+          },
+        ],
+      },
+    ],
+  };
   /* const icon = {
     url: require('../restaurant icon.png'),
     scaledSize: new window.google.maps.Size(90, 42)
   } */
   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
+    id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries
-  })
-  const [map, setMap] = React.useState(null)
-  const mapRef = React.useRef()
+    libraries,
+  });
+  const [map, setMap] = React.useState(null);
+  const mapRef = React.useRef();
   /* const request = {
     location: currentPos,
     radius: '2000',
     type: ['restaurant']
   } */
 
-  const onLoad = React.useCallback(function callback (map) {
+  const onLoad = React.useCallback(function callback(map) {
     /* const bounds = new window.google.maps.LatLngBounds(defaultCenter)
     map.fitBounds(bounds) */
-    setMap(map)
-    mapRef.current = map
-  }, [])
+    setMap(map);
+    mapRef.current = map;
+  }, []);
 
-  const onUnmount = React.useCallback(function callback (map) {
-    setMap(null)
-  }, [])
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
   /* const nearbySearch = React.useCallback(function callback (results, status) {
     if (status === window.google.maps.places.PlacesServiceStatus.OK) {
       for (let i = 0; i < results.length; i++) {
@@ -110,57 +110,58 @@ const Map = () => {
       if (navigator.geolocation) {
         const currentPosition = {
           lat: position.coords.latitude,
-          lng: position.coords.longitude
-        }
-        setCurrentPos(currentPosition)
+          lng: position.coords.longitude,
+        };
+        setCurrentPos(currentPosition);
       }
-    }
-    navigator.geolocation.getCurrentPosition(getPos)
-  }, [])
+    };
+    navigator.geolocation.getCurrentPosition(getPos);
+  }, []);
   const panToLocation = () => {
-    setClick(true)
-    console.log('Current', currentPos)
+    setClick(true);
+    console.log("Current", currentPos);
     if (currentPos !== {}) {
       const request = {
         location: currentPos,
-        radius: '2000',
-        type: ['restaurant']
-      }
+        radius: "2000",
+        type: ["restaurant"],
+      };
 
-      const service = new window.google.maps.places.PlacesService(mapRef.current)
-      service.nearbySearch(request, callback)
+      const service = new window.google.maps.places.PlacesService(
+        mapRef.current
+      );
+      service.nearbySearch(request, callback);
 
-      function callback (results, status, pagination) {
+      function callback(results, status, pagination) {
         if (status === window.google.maps.places.PlacesServiceStatus.OK) {
           for (let i = 0; i < results.length; i++) {
-            createMarker(results[i])
+            createMarker(results[i]);
           }
           if (pagination && pagination.hasNextPage) {
-            pagination.nextPage()
+            pagination.nextPage();
             setTimeout(function () {
-              setNextPage(true)
-            }, 3000)
+              setNextPage(true);
+            }, 3000);
           }
         }
       }
     }
-  }
+  };
   const createMarker = (place) => {
-    console.log('Place', place)
-    placesList.push(place)
-    setPlacesFinal(placesList)
-  }
+    console.log("Place", place);
+    placesList.push(place);
+    setPlacesFinal(placesList);
+  };
 
   const isRestaurantOpenBool = true;
   let isRestaurantOpen = "Open";
   let restaurantName = "Dreams Cafe";
   let restaurantAddress = "Karaportti 4, 02610 Espoo";
-  
-  return isLoaded
-    ? (
-      <div className="app">
+
+  return isLoaded ? (
+    <div className="app">
       <div className="container">
-        {/* <button
+        <button
           type="button"
           onClick={() => {
             setIsOpen(!isOpen);
@@ -168,7 +169,7 @@ const Map = () => {
           }}
         >
           Trigger Drawer
-        </button> */}
+        </button>
         <GoogleMap
           id="map"
           mapContainerStyle={mapStyles}
@@ -182,10 +183,68 @@ const Map = () => {
           onLoad={onLoad}
           onUnmount={onUnmount}
         >
-          <Button onClick={panToLocation} style={{ paddingLeft: 250 }}>
-            Current Location
-          </Button>
+          <IconButton
+            onClick={() => panToLocation()}
+            style={{ marginLeft: 250 }}
+            color={"primary"}
+          >
+            <MyLocationIcon />
+          </IconButton>
           {console.log(map)}
+          {
+            <Marker
+              icon={"https://www.robotwoods.com/dev/misc/bluecircle.png"}
+              position={currentPos}
+            />
+          }
+          {checkClick
+            ? placesFinal.map(function (results) {
+                return (
+                  <Marker
+                    clickable={true}
+                    icon={{
+                      url: require("../restaurant icon.png"),
+                      scaledSize: new window.google.maps.Size(50, 42),
+                    }}
+                    key={results.place_id}
+                    position={{
+                      lat: results.geometry.location.lat(),
+                      lng: results.geometry.location.lng(),
+                    }}
+                  >
+                    {/* <InfoWindow
+                      position={{ lat: results.geometry.location.lat(), lng: results.geometry.location.lng() }}
+                      options={{ maxWidth: 300 }}>
+                      <span>{results.name}</span>
+                    </InfoWindow> */}
+                  </Marker>
+                );
+              })
+            : console.log("nothing", "nothing")}
+          {checkNextPage
+            ? placesFinal.map(function (results) {
+                return (
+                  <Marker
+                    clickable={true}
+                    icon={{
+                      url: require("../restaurant icon.png"),
+                      scaledSize: new window.google.maps.Size(50, 42),
+                    }}
+                    key={results.place_id}
+                    position={{
+                      lat: results.geometry.location.lat(),
+                      lng: results.geometry.location.lng(),
+                    }}
+                  >
+                    {/* <InfoWindow
+                    position={{ lat: results.geometry.location.lat(), lng: results.geometry.location.lng() }}
+                    options={{ maxWidth: 300 }}>
+                    <span>{results.name}</span>
+                  </InfoWindow> */}
+                  </Marker>
+                );
+              })
+            : console.log("nothing", "nothing")}
           <></>
         </GoogleMap>
 
@@ -216,8 +275,8 @@ const Map = () => {
                       ? "#DAF7A6"
                       : "#FF8266",
                     fontSize: "4vw",
-                    width: '50%',
-                    padding: '5px',
+                    width: "50%",
+                    padding: "5px",
                     borderRadius: 12,
                   }}
                 >
@@ -228,19 +287,18 @@ const Map = () => {
               </div>
               <div style={iconContainerStyle}>
                 <Box sx={iconBoxStyle}>
-                <img
-                  src={restaurantMenuLogo}
-                  alt="Restaurant menu logo"
-                  style={{height: '100%', width: '100%'}}
-                  
-                />
+                  <img
+                    src={restaurantMenuLogo}
+                    alt="Restaurant menu logo"
+                    style={{ height: "100%", width: "100%" }}
+                  />
                 </Box>
                 <Box sx={iconBoxStyle}>
-                <img
-                  src={restaurantDirectionsLogo}
-                  alt="Restaurant menu logo"
-                  style={{height: '100%', width: '100%'}}
-                />
+                  <img
+                    src={restaurantDirectionsLogo}
+                    alt="Restaurant menu logo"
+                    style={{ height: "100%", width: "100%" }}
+                  />
                 </Box>
               </div>
             </div>
@@ -267,7 +325,7 @@ const style = {
 
 const textStyle = {
   fontSize: "3vw",
-  padding: '5px',
+  padding: "5px",
 };
 
 const iconContainerStyle = {

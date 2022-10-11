8f7d0772aee8ca7099fa4cf896bcfaedac050556
irina <irina.konovalova455@gmail.com>
2022-10-11 19:49:25 +0300

    refactoring home and menu pages, loading menu componentnt

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,15 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { List } from '@mui/material'
 import RestaurantMenuItem from '../components/RestaurantMenuItem'
+import { useCookies } from 'react-cookie'
 
 const RestaurantMenu = ({ menu, restaurantType }) => {
+  const [cookies] = useCookies(['language'])
   return (
     <List>
       {restaurantType === 'sodexo' &&
         Object.values(menu).map((item, index) => (
           <RestaurantMenuItem
             key={index}
-            menuItem={item?.title_en}
+            menuItem={cookies.language === 'en' ? item?.title_en : item?.title_fi}
             mealType={item?.category}
             price={item?.price}
             dietInfo={item?.additionalDietInfo?.allergens}

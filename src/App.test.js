c9d59e0f64b01580bcf0d7fb5b210ce0b97bd66e
Teme8 <teemu.tirkkonen@metropolia.fi>
2022-10-24 16:17:00 +0300

    App.test fix

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 
 // eslint-disable-next-line no-undef
-test('renders learn react link', () => {
+test('renders MyRestaurant link', () => {
   render(<App />)
   const linkElement = screen.getByRole('link', { name: /MyRestaurant/ })
   // eslint-disable-next-line no-undef

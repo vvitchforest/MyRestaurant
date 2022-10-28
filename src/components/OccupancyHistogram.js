8a36ee7169740393f67a838f0290518b205a59eb
oskar wiiala <oskar.wiiala@metropolia.fi>
2022-10-28 16:29:45 +0300

    randomized occupancy data for occupancy histogram based on day and time

--- src/components/OccupancyHistogram.js
+++ src/components/OccupancyHistogram.js
@@ -16,18 +16,33 @@ import {
   CommonSeriesSettings,
   Size
 } from 'devextreme-react/chart'
-import { occupancyData } from '../utils/OccupancyData.js'
+import { getOccupancyData } from '../utils/OccupancyData.js'
 import { useCookies } from 'react-cookie'
 import getTranslation from '../utils/Translations'
 
 // args should only contain width and height
 const OccupancyHistogram = (args) => {
+  const getCurrentTimeQuartered = () => {
+    const date = new Date()
+    const hour = date.getHours().toString()
+    const minute = date.getMinutes()
+    let convertedMinute = ''
+    if (minute < 30 && minute >= 15) {
+      convertedMinute = ':15'
+    } else if (minute < 45 && minute >= 30) {
+      convertedMinute = ':30'
+    } else if (minute >= 45) {
+      convertedMinute = ':45'
+    }
+    return hour + convertedMinute
+  }
+
   const [cookies] = useCookies(['language'])
   const [highAverage] = useState(40)
   const [lowAverage] = useState(15)
   const [width, setWidth] = useState()
   const [height, setHeight] = useState()
-  const currentHour = new Date().getHours()
+  const currentTimeQuarter = getCurrentTimeQuartered()
 
   useEffect(() => {
     console.log('occupancy histogram: ', args)
@@ -35,9 +50,29 @@ const OccupancyHistogram = (args) => {
     setHeight(args.height ? args.height : 150)
   })
 
+  const getCurrentDay = () => {
+    const days = [
+      'sunday',
+      'monday',
+      'tuesday',
+      'wednesday',
+      'thursday',
+      'friday',
+      'saturday'
+    ]
+    const d = new Date()
+    return days[d.getDay()]
+  }
+
   // Customizes actual value and prediction value color for bar when it is at current hour
   const customizePoint = (arg) => {
-    if (arg.data.hour === currentHour.toString()) {
+    console.log(
+      'data time:',
+      arg.data.time,
+      'current time quartered:',
+      currentTimeQuarter
+    )
+    if (arg.data.time === currentTimeQuarter) {
       // Series 1 pertains to actual value
       if (arg.seriesName === 'Series 1') {
         return {
@@ -57,66 +92,66 @@ const OccupancyHistogram = (args) => {
   }
 
   return (
-      <Chart
-        id='chart'
-        title='Occupancy histogram'
-        dataSource={occupancyData}
-        customizePoint={customizePoint}
+    <Chart
+      id='chart'
+      title='Occupancy histogram'
+      dataSource={getOccupancyData(getCurrentDay())}
+      customizePoint={customizePoint}
+    >
+      <Size width={width} height={height} />
+      <CommonSeriesSettings
+        argumentField='time'
+        type='bar'
+        barOverlapGroup='time'
+      />
+      <Series id='seriesActual' valueField='valueActual' color='#1976D2' />
+      <Series
+        id='seriesPrediction'
+        valueField='valuePrediction'
+        color='#1976D24D'
+      />
+      <ArgumentAxis
+        title={getTranslation(
+          cookies.language ? cookies.language : 'en',
+          'hourofday'
+        )}
+      />
+      <ValueAxis
+        title={getTranslation(
+          cookies.language ? cookies.language : 'en',
+          'people'
+        )}
       >
-        <Size width={width} height={height} />
-        <CommonSeriesSettings
-          argumentField='hour'
-          type='bar'
-          barOverlapGroup='hour'
-        />
-        <Series id='seriesActual' valueField='valueActual' color='#1976D2' />
-        <Series
-          id='seriesPrediction'
-          valueField='valuePrediction'
-          color='#1976D24D'
-        />
-        <ArgumentAxis
-          title={getTranslation(
-            cookies.language ? cookies.language : 'en',
-            'hourofday'
-          )}
-        />
-        <ValueAxis
-          title={getTranslation(
-            cookies.language ? cookies.language : 'en',
-            'people'
-          )}
+        <VisualRange startValue={0} />
+        <ConstantLine
+          width={2}
+          value={lowAverage}
+          color='#8c8cff'
+          dashStyle='dash'
         >
-          <VisualRange startValue={0} />
-          <ConstantLine
-            width={2}
-            value={lowAverage}
-            color='#8c8cff'
-            dashStyle='dash'
-          >
-            <Label
-              text={getTranslation(
-                cookies.language ? cookies.language : 'en',
-                'lowaverage'
-              )}
-            />
-          </ConstantLine>
-          <ConstantLine
-            width={2}
-            value={highAverage}
-            color='#ff7c7c'
-            dashStyle='dash'
-          >
-            <Label
-              text={getTranslation(
-                cookies.language ? cookies.language : 'en',
-                'highaverage'
-              )}
-            />
-          </ConstantLine>
-        </ValueAxis>
-        <Legend visible={false} />
-      </Chart>
+          <Label
+            text={getTranslation(
+              cookies.language ? cookies.language : 'en',
+              'lowaverage'
+            )}
+          />
+        </ConstantLine>
+        <ConstantLine
+          width={2}
+          value={highAverage}
+          color='#ff7c7c'
+          dashStyle='dash'
+        >
+          <Label
+            text={getTranslation(
+              cookies.language ? cookies.language : 'en',
+              'highaverage'
+            )}
+          />
+        </ConstantLine>
+      </ValueAxis>
+      <Legend visible={false} />
+    </Chart>
   )
 }
 

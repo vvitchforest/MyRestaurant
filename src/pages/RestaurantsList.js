f1d2b991245f72af74611d5c49980ce30adc3880
irina <irina.konovalova455@gmail.com>
2022-11-11 11:49:14 +0200

    radius

--- src/pages/RestaurantsList.js
+++ src/pages/RestaurantsList.js
@@ -78,7 +78,7 @@ const RestaurantsList = () => {
     if (currentPos !== {}) {
       const request = {
         location: currentPos,
-        radius: '500',
+        radius: '50',
         type: ['restaurant']
       }
       // Gets the Google PlacesService and sets it to invisible div element
